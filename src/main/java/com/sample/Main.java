95d5ef9726c7a9259da4fab58347c8503c0f1dec
unknown <u26535@A639LCOK.ustr.com>
2015-04-20 17:00:29 +0530

    Initial version

---
+++ src/main/java/com/sample/Main.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.sample;
+
+/**
+ * @author u26535
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome Rakesh");
+
+	}
+
+}
